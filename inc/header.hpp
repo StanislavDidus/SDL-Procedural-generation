8942530b25a8cbb36f24d15cf95624bbf5b10a48
StanislavDidus <stasdidus9@gmail.com>
2025-11-16 13:01:02 +0100

    Cleared solution folder

---
+++ inc/header.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+void countFrom1To10()
+{
+	for (int i = 1; i < 11; i++)
+	{
+		std::cout << i << std::endl;
+	}
+}
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "header.hpp"
+
+int main()
+{
+	std::cout << "Program was initialized successfully!" << std::endl;
+
+	countFrom1To10();
+
+	return 0;
+}
