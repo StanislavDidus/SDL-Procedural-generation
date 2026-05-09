1f20c05d71bbffd2da930e80f2a2cd2e8fb7f404
Stanislav Didus <stasdidus9@gmail.com>
2026-05-09 12:50:43 +0200

    Merged new changes

---
+++ inc/GpuRenderFunctions.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <graphics/GpuRenderer.hpp>
+
+static bool IGNORE_VIEW_ZOOM = true;
+
+inline glm::vec2 getMouseGlobalPosition(const graphics::GpuRenderer& renderer, const glm::vec2& mouse_position)
+{
+    const auto& view = renderer.getView();
+    const auto& zoom = renderer.getZoom();
+    const auto& window_size = renderer.getStandardWindowSize();
+
+    glm::vec2 mid_screen {window_size.x * 0.5f, window_size.y * 0.5f};
+
+    glm::vec2 result{ view + mid_screen + (mouse_position - mid_screen) / zoom };
+    return result;
+}
+
+inline void drawRectangle(graphics::GpuRenderer& screen, float x, float y, float width, float height, graphics::RenderType render_type, graphics::Color color, bool ignore_view_zoom = false)
+{
+    if (ignore_view_zoom)
+        screen.setRenderMode(graphics::RenderMode::UI);
+    graphics::Color new_color{static_cast<uint8_t>(color.r / 255.0f), static_cast<uint8_t>(color.g / 255.0f), static_cast<uint8_t>(color.b / 255.0f), static_cast<uint8_t>(color.a / 255.0f)};
+    screen.renderRectangle(x, y, width, height, render_type, new_color);
+    if (ignore_view_zoom)
+        screen.setRenderMode(graphics::RenderMode::WORLD);
+}
+
+inline void drawScaledSprite(graphics::GpuRenderer& screen, const graphics::Sprite& sprite, float x, float y, float width, float height, bool ignore_view_zoom = false, graphics::Color color = graphics::Color::WHITE)
+{
+    if (ignore_view_zoom)
+        screen.setRenderMode(graphics::RenderMode::UI);
+    graphics::Color new_color{static_cast<uint8_t>(color.r / 255.0f), static_cast<uint8_t>(color.g / 255.0f), static_cast<uint8_t>(color.b / 255.0f), static_cast<uint8_t>(color.b / 255.0f)};
+    screen.renderSprite(sprite, x, y, width, height, 0.0f, SDL_FLIP_NONE, color);
+    if (ignore_view_zoom) 
+        screen.setRenderMode(graphics::RenderMode::WORLD);
+}
+
+inline void drawRotatedSprite(graphics::GpuRenderer& screen, const graphics::Sprite& sprite, float x, float y, float width, float height, float angle,
+    SDL_FlipMode flip_mode, bool ignore_view_zoom = false, graphics::Color color = graphics::Color::WHITE)
+{
+    if (ignore_view_zoom) 
+        screen.setRenderMode(graphics::RenderMode::UI);
+    graphics::Color new_color{static_cast<uint8_t>(color.r / 255.0f), static_cast<uint8_t>(color.g / 255.0f), static_cast<uint8_t>(color.b / 255.0f), static_cast<uint8_t>(color.b / 255.0f)};
+    screen.renderSprite(sprite, x, y, width, height, angle, flip_mode, color);
+    if (ignore_view_zoom) 
+        screen.setRenderMode(graphics::RenderMode::WORLD);
+}
+
+inline void printText(graphics::GpuRenderer& screen, const graphics::Text& text, float x, float y, float w, float h, bool ignore_view_zoom = false)
+{
+    if (ignore_view_zoom) 
+        screen.setRenderMode(graphics::RenderMode::UI);
+    screen.renderText(text, x, y, w, h);
+    if (ignore_view_zoom) 
+        screen.setRenderMode(graphics::RenderMode::WORLD);
+}
+
+inline void printTextScaled(graphics::GpuRenderer& screen, const graphics::Text& text, float x, float y, float scale_x, float scale_y, bool ignore_view_zoom = false)
+{
+    if (ignore_view_zoom) 
+        screen.setRenderMode(graphics::RenderMode::UI);
+    glm::vec2 scaled_size = text.getTextSize({scale_x, scale_y});
+    screen.renderText(text, x, y, scaled_size.x, scaled_size.y);
+    if (ignore_view_zoom)
+        screen.setRenderMode(graphics::RenderMode::WORLD);
+}
